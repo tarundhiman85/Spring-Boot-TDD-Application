2da69ec70ac473075e8c24eceb7f3e2ad7322c29
TarunDhiman12 <118983395+TarunDhiman12@users.noreply.github.com>
2025-01-03 14:39:54 +0530

    Initial Commit added Dependencies

---
+++ src/main/java/com/tarunspringboottldd/springboottldd/SpringBootTlddApplication.java
@@ -0,0 +1,13 @@
+package com.tarunspringboottldd.springboottldd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTlddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTlddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tarunspringboottldd/springboottldd/SpringBootTlddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarunspringboottldd.springboottldd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTlddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
