c7f0c5e2e3535f9a14a2645de4f1d2454130206a
TarunDhiman12 <118983395+TarunDhiman12@users.noreply.github.com>
2025-01-03 14:46:04 +0530

    test: add failing test for greeting message functionality

---
+++ src/main/java/com/tarunspringboottldd/springboottldd/Controller/GreetController.java
@@ -0,0 +1,15 @@
+package com.tarunspringboottldd.springboottldd.Controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greet")
+public class GreetController {
+
+        @RequestMapping("/hello/{name}")
+        public String sayHello(@PathVariable String name) {
+            return "Hello, Welcome to Spring Boot";
+        }
+}
--- src/test/java/com/tarunspringboottldd/springboottldd/GreetTest.java
+++ src/test/java/com/tarunspringboottldd/springboottldd/GreetTest.java
@@ -1,4 +1,17 @@
 package com.tarunspringboottldd.springboottldd;
 
+import static org.junit.Assert.assertEquals;
+
+import com.tarunspringboottldd.springboottldd.Controller.GreetController;
+import org.junit.Test;
+
+
 public class GreetTest {
+
+    @Test
+    public void testSayHello() {
+        GreetController greetController = new GreetController();
+        String result = greetController.sayHello("John");
+        assertEquals("Hello, John Welcome to Spring Boot", result);
+    }
 }
