7258cf410589dc89fb992d8422171230818fee91
TarunDhiman12 <118983395+TarunDhiman12@users.noreply.github.com>
2025-01-04 17:06:27 +0530

    feat: support custom delimiters in Add method

--- src/main/java/com/tarunspringboottldd/springboottldd/Helpers/StringHelpers.java
+++ src/main/java/com/tarunspringboottldd/springboottldd/Helpers/StringHelpers.java
@@ -12,7 +12,15 @@ public class StringHelpers {
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] parts = numbers.split("[,\n]");
+
+        String delimiter = "[,\n]";
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numbers = numbers.substring(delimiterIndex + 1);
+        }
+
+        String[] parts = numbers.split(delimiter);
         int sum = 0;
         for (String part : parts) {
             sum += Integer.parseInt(part);
