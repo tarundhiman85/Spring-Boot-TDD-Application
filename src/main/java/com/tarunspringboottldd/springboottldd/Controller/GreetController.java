374aae06f56d940bfdffaadfe94cdf46bee170e4
TarunDhiman12 <118983395+TarunDhiman12@users.noreply.github.com>
2025-01-04 16:06:22 +0530

    refractor: changed helper class name to string Helper

--- src/main/java/com/tarunspringboottldd/springboottldd/Controller/GreetController.java
+++ src/main/java/com/tarunspringboottldd/springboottldd/Controller/GreetController.java
@@ -1,5 +1,5 @@
 package com.tarunspringboottldd.springboottldd.Controller;
-import com.tarunspringboottldd.springboottldd.Helpers.Helpers;
+import com.tarunspringboottldd.springboottldd.Helpers.StringHelpers;
 import com.tarunspringboottldd.springboottldd.Constants.MessageConstants;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +11,7 @@ public class GreetController {
 
         @RequestMapping("/hello/{name}")
         public String sayHello(@PathVariable String name) {
-            new Helpers().validateName(name);
+            new StringHelpers().validateName(name);
             return String.format(MessageConstants.GREETING_MESSAGE, name);
         }
 
--- src/main/java/com/tarunspringboottldd/springboottldd/Helpers/StringHelpers.java
+++ src/main/java/com/tarunspringboottldd/springboottldd/Helpers/StringHelpers.java
@@ -2,7 +2,7 @@ package com.tarunspringboottldd.springboottldd.Helpers;
 
 import com.tarunspringboottldd.springboottldd.Constants.MessageConstants;
 
-public class Helpers {
+public class StringHelpers {
     public void validateName(String name) {
         if (name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException(MessageConstants.EMPTY_NAME_ERROR);
