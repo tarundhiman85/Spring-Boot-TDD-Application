3eb71f08e860c74febc02b43ff1e3f15dadf128c
TarunDhiman12 <118983395+TarunDhiman12@users.noreply.github.com>
2025-01-04 16:21:28 +0530

    test: add failing test for empty string input

--- src/test/java/com/tarunspringboottldd/springboottldd/StringHelpersTest.java
+++ src/test/java/com/tarunspringboottldd/springboottldd/StringHelpersTest.java
@@ -1,4 +1,14 @@
 package com.tarunspringboottldd.springboottldd;
 
+import com.tarunspringboottldd.springboottldd.Helpers.StringHelpers;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringHelpersTest {
+    @Test
+    public void testAdd_EmptyString_ShouldReturnZero() {
+        StringHelpers calculator = new StringHelpers();
+        assertEquals(0, calculator.Add(""));
+    }
 }
